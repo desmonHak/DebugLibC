2da5adf5be669b52bfeee1a6eea00831db2e2ea7
desmon <desmonHak@users.noreply.github.com>
2025-02-10 12:25:04 +0100

    update debug

--- debug_c.h
+++ debug_c.h
@@ -30,120 +30,120 @@
  *	modificado.
  */
 
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
-
-#include <stdio.h>
-#include <stdarg.h>
-#include "colors.h"
-#include "error_c.h"
-
-#pragma comment(lib, "libcolors.a")
-#pragma comment(lib, "libdebug.a")
-
-//#include "WL_lib.h"
-//#define NAME_DEFAULT_LOG_DEBUG "debug_log.txt"
-//MyFile Log_debug_file;
-
-#ifdef DEBUG_ENABLE
-#define DEBUG_PRINT(level, fmt, ...) debug_print(level, "#{FG:lgreen}[#{FG:lpurple}%s#{reset}:#{FG:cyan}%s#{reset}:#{FG:red}%03d#{FG:green}]#{reset} " fmt , get_level_debug(level), __FILE__, __LINE__, ##__VA_ARGS__)
-
-#else
-#define DEBUG_PRINT(level, fmt, ...)
-#endif
-
-
-#define INIT_TYPE_FUNC_DBG(type_return, name_func) "#{FG:lred} "#type_return" #{FG:cyan}"#name_func"#{FG:white}("
-#define TYPE_DATA_DBG(type_data, name_arg) " #{FG:lyellow}"#type_data" #{FG:white} "name_arg", "
-#define END_TYPE_FUNC_DBG "#{FG:white}); #{reset}\n"
-
-typedef enum
-{
-    DEBUG_LEVEL_INFO,
-    DEBUG_LEVEL_WARNING,
-    DEBUG_LEVEL_ERROR
-} DebugLevel;
-static DebugLevel currentLevel = DEBUG_LEVEL_INFO; // nivel de debbug del programa por default
-
-static const char* ErrorLevelStrings[] = {
-    "INFO",
-    "WARNING",
-    "ERROR",
-};
-
-typedef struct {
-    char *message;
-    const char *file;
-    unsigned int line;
-    DebugLevel level_c;
-} Debug;
-
-#define DEBUG_C(level, msg)                                                                 \
-    do                                                                                    \
-    {                                                                                     \
-        Debug Deb = {.message = msg, .file = __FILE__, .line = __LINE__, .level_c = level}; \
-        debug_print(Deb.level_c, Deb.message);     \
- } while (0)
-
-#ifdef DEBUG_ENABLE
-#define debug_malloc(type, name_var, size) \
-        name_var = (type*)malloc(size); \
-        DEBUG_PRINT(DEBUG_LEVEL_INFO, "#{FG:red}[#{FG:yellow}MALLOC#{FG:red}]#{FG:lred}  #{FG:white}(#{FG:lred}%s#{FG:white}*)#{FG:cyan}malloc#{FG:white}(%d) #{FG:lgreen}Variable#{FG:white}: %s, #{FG:lgreen}Tipo#{FG:white}: %s, #{FG:lgreen}Puntero#{FG:white}: %p\n", #type, size, #name_var, #type, name_var); \
-        if (NULL == name_var) puts("malloc error");
-#else
-#define debug_malloc(type, name_var, size) name_var = (type*)malloc(size)
-#endif
-#ifdef DEBUG_ENABLE
-#define debug_calloc(type, name_var, cantidad, size) \
-        name_var = (type*)calloc(cantidad, size); \
-        DEBUG_PRINT(DEBUG_LEVEL_INFO, "#{FG:red}[#{FG:yellow}CALLOC#{FG:red}] #{FG:white}(#{FG:lred}%s#{FG:white}*)#{FG:cyan}calloc#{FG:white}(%d, %d) #{FG:lgreen}Variable#{FG:white}: %s, #{FG:lgreen}Tipo#{FG:white}: %s, #{FG:lgreen}Puntero#{FG:white}: %p\n", #type, cantidad, size, #name_var, #type, name_var); \
-        if (NULL == name_var) puts("calloc error");
-#else
-#define debug_calloc(type, name_var, cantidad, size) name_var = (type*)calloc(cantidad, size); if (NULL == name_var) puts("calloc error");
-#endif
-
-#ifdef DEBUG_ENABLE
-#define debug_realloc(type, name_var, size) \
-        name_var = (type*)realloc(name_var, size); \
-        DEBUG_PRINT(DEBUG_LEVEL_INFO, "#{FG:red}[#{FG:yellow}REALLOC#{FG:red}] #{FG:white}(#{FG:lred}%s#{FG:white}*)#{FG:cyan}realloc#{FG:white}(%p, %d) #{FG:lgreen}Variable#{FG:white}: %s, #{FG:lgreen}Tipo#{FG:white}: %s, #{FG:lgreen}Puntero#{FG:white}: %p\n", #type, name_var, size, #name_var, #type, name_var); \
-        if (NULL == name_var) puts("realloc error");
-#else
-#define debug_realloc(type, name_var, size) name_var = (type*)realloc(name_var, size); if (NULL == name_var) puts("realloc error");
-#endif
-
-const char* get_level_debug(DebugLevel level);
-//void debug_set_log_file(const char *filename);
-void debug_set_level(DebugLevel level);
-void debug_print(DebugLevel level, const char *fmt, ...);
-#ifdef _MSC_VER
-void __constructor_debug_c__();
-void __destructor_debug_c__();
-#else
-void __attribute__((constructor)) __constructor_debug_c__();
-void __attribute__((destructor)) __destructor_debug_c__();
-#endif
-
-#ifdef _WIN32
-#ifndef _ExceptionHandler_WIN_
-#define _ExceptionHandler_WIN_
-LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS *ExceptionInfo);
-#endif
-#else
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <signal.h>
-#include <ucontext.h>
-#include <execinfo.h>
-#include <unistd.h>
-#include <inttypes.h>
-void signalHandler(int sig, siginfo_t *info, void *context);
-#endif
-
-//static FILE *logFile = NULL;
-
-#ifdef INCLUDE_COLORS_C
-#include "debug_c.c"
-#endif
-#endif
+ #ifndef __DEBUG_H__
+ #define __DEBUG_H__
+ 
+ #include <stdio.h>
+ #include <stdarg.h>
+ #include "colors.h"
+ #include "error_c.h"
+ 
+ #pragma comment(lib, "libcolors.a")
+ #pragma comment(lib, "libdebug.a")
+ 
+ //#include "WL_lib.h"
+ //#define NAME_DEFAULT_LOG_DEBUG "debug_log.txt"
+ //MyFile Log_debug_file;
+ 
+ #ifdef DEBUG_ENABLE
+ #define DEBUG_PRINT(level, fmt, ...) debug_print(level, "#{FG:lgreen}[#{FG:lpurple}%s#{reset}:#{FG:cyan}%s#{reset}:#{FG:red}%03d#{FG:green}]#{reset} " fmt , get_level_debug(level), __FILE__, __LINE__, ##__VA_ARGS__)
+ 
+ #else
+ #define DEBUG_PRINT(level, fmt, ...) if (level == DEBUG_LEVEL_ERROR) debug_print(level, "#{FG:lgreen}[#{FG:lpurple}%s#{reset}:#{FG:cyan}%s#{reset}:#{FG:red}%03d#{FG:green}]#{reset} " fmt , get_level_debug(level), __FILE__, __LINE__, ##__VA_ARGS__)
+ #endif
+ 
+ 
+ #define INIT_TYPE_FUNC_DBG(type_return, name_func) "#{FG:lred} "#type_return" #{FG:cyan}"#name_func"#{FG:white}("
+ #define TYPE_DATA_DBG(type_data, name_arg) " #{FG:lyellow}"#type_data" #{FG:white} "name_arg", "
+ #define END_TYPE_FUNC_DBG "#{FG:white}); #{reset}\n"
+ 
+ typedef enum
+ {
+     DEBUG_LEVEL_INFO,
+     DEBUG_LEVEL_WARNING,
+     DEBUG_LEVEL_ERROR
+ } DebugLevel;
+ static DebugLevel currentLevel = DEBUG_LEVEL_INFO; // nivel de debbug del programa por default
+ 
+ static const char* ErrorLevelStrings[] = {
+     "INFO",
+     "WARNING",
+     "ERROR",
+ };
+ 
+ typedef struct {
+     char *message;
+     const char *file;
+     unsigned int line;
+     DebugLevel level_c;
+ } Debug;
+ 
+ #define DEBUG_C(level, msg)                                                                 \
+     do                                                                                    \
+     {                                                                                     \
+         Debug Deb = {.message = msg, .file = __FILE__, .line = __LINE__, .level_c = level}; \
+         debug_print(Deb.level_c, Deb.message);     \
+  } while (0)
+ 
+ #ifdef DEBUG_ENABLE
+ #define debug_malloc(type, name_var, size) \
+         name_var = (type*)malloc(size); \
+         DEBUG_PRINT(DEBUG_LEVEL_INFO, "#{FG:red}[#{FG:yellow}MALLOC#{FG:red}]#{FG:lred}  #{FG:white}(#{FG:lred}%s#{FG:white}*)#{FG:cyan}malloc#{FG:white}(%d) #{FG:lgreen}Variable#{FG:white}: %s, #{FG:lgreen}Tipo#{FG:white}: %s, #{FG:lgreen}Puntero#{FG:white}: %p\n", #type, size, #name_var, #type, name_var); \
+         if (NULL == name_var) DEBUG_PRINT(DEBUG_LEVEL_ERROR,"malloc error\n");
+ #else
+ #define debug_malloc(type, name_var, size) name_var = (type*)malloc(size)
+ #endif
+ #ifdef DEBUG_ENABLE
+ #define debug_calloc(type, name_var, cantidad, size) \
+         name_var = (type*)calloc(cantidad, size); \
+         DEBUG_PRINT(DEBUG_LEVEL_INFO, "#{FG:red}[#{FG:yellow}CALLOC#{FG:red}] #{FG:white}(#{FG:lred}%s#{FG:white}*)#{FG:cyan}calloc#{FG:white}(%d, %d) #{FG:lgreen}Variable#{FG:white}: %s, #{FG:lgreen}Tipo#{FG:white}: %s, #{FG:lgreen}Puntero#{FG:white}: %p\n", #type, cantidad, size, #name_var, #type, name_var); \
+         if (NULL == name_var) DEBUG_PRINT(DEBUG_LEVEL_ERROR,"calloc error\n");
+ #else
+ #define debug_calloc(type, name_var, cantidad, size) name_var = (type*)calloc(cantidad, size); if (NULL == name_var) DEBUG_PRINT(DEBUG_LEVEL_ERROR,"calloc error\n");
+ #endif
+ 
+ #ifdef DEBUG_ENABLE
+ #define debug_realloc(type, name_var, size) \
+         name_var = (type*)realloc(name_var, size); \
+         DEBUG_PRINT(DEBUG_LEVEL_INFO, "#{FG:red}[#{FG:yellow}REALLOC#{FG:red}] #{FG:white}(#{FG:lred}%s#{FG:white}*)#{FG:cyan}realloc#{FG:white}(%p, %d) #{FG:lgreen}Variable#{FG:white}: %s, #{FG:lgreen}Tipo#{FG:white}: %s, #{FG:lgreen}Puntero#{FG:white}: %p\n", #type, name_var, size, #name_var, #type, name_var); \
+         if (NULL == name_var) DEBUG_PRINT(DEBUG_LEVEL_ERROR,"realloc error\n");
+ #else
+ #define debug_realloc(type, name_var, size) name_var = (type*)realloc(name_var, size); if (NULL == name_var) DEBUG_PRINT(DEBUG_LEVEL_ERROR,"realloc error\n");
+ #endif
+ 
+ const char* get_level_debug(DebugLevel level);
+ //void debug_set_log_file(const char *filename);
+ void debug_set_level(DebugLevel level);
+ void debug_print(DebugLevel level, const char *fmt, ...);
+ #ifdef _MSC_VER
+ void __constructor_debug_c__();
+ void __destructor_debug_c__();
+ #else
+ void __attribute__((constructor)) __constructor_debug_c__();
+ void __attribute__((destructor)) __destructor_debug_c__();
+ #endif
+ 
+ #ifdef _WIN32
+ #ifndef _ExceptionHandler_WIN_
+ #define _ExceptionHandler_WIN_
+ LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS *ExceptionInfo);
+ #endif
+ #else
+ #define _GNU_SOURCE
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <stdint.h>
+ #include <signal.h>
+ #include <ucontext.h>
+ #include <execinfo.h>
+ #include <unistd.h>
+ #include <inttypes.h>
+ void signalHandler(int sig, siginfo_t *info, void *context);
+ #endif
+ 
+ //static FILE *logFile = NULL;
+ 
+ #ifdef INCLUDE_COLORS_C
+ #include "debug_c.c"
+ #endif
+ #endif
